f5453cf10b5de21916e495763ba8199fa3d92eb2
Allison Mackey <a.mackey693@gmail.com>
2020-05-15 08:51:21 -0700

    add logic to pass test on spec #1, test PASSES

--- lib/word.rb
+++ lib/word.rb
@@ -1,2 +1,13 @@
 require 'pry'
 
+class Word  
+  attr_reader :id
+  attr_accessor :word
+  @@words = {}
+  @@total_rows = 0
+
+  def initialize(attributes)
+    @word = attributes.fetch(:word)
+    @id = attributes.fetch(:id) || @@total_rows += 1
+  end
+end
