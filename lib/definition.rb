3d5dadaf4685aed1fbb6570383ea94c33c29a922
Allison Mackey <a.mackey693@gmail.com>
2020-05-15 11:22:00 -0700

    add logic to pass test on spec #1 of #Def, test PASSES

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,1 +1,18 @@
 require 'pry'
+require 'word'
+
+class Def
+  attr_reader :id, :timestamp
+  attr_accessor :definition, :author, :word_id
+
+  @@definitions = {}
+  @@total_rows = 0 
+
+  def initialize(attributes)
+    @definition = attributes.fetch(:definition)
+    @author = attributes.fetch(:author)
+    @word_id = word_id
+    @id = attributes.fetch(:id) || @@total_rows += 1
+    @timestamp = Time.new()
+  end
+end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -4,7 +4,9 @@ require 'pry'
 
 describe '#Def' do
   describe('#initialize')do
-    def1 = Def.new({ :definition =>"a definition here", :author => "johnny " :id => nil)
-    expect(def1.definition).to(eq("a definition here"))
+    it('creates a hash with attributes')do
+      def1 = Def.new({ :definition =>"a definition here", :author => "johnny ", :id => nil })
+      expect(def1.definition).to(eq("a definition here"))
+    end
   end
 end
